2cf803958bfd50296fc7a30b219c7d2579b51baf
Bilisuma <bilitade2022@gmail.com>
2025-02-24 13:33:54 +0300

    status bar hidden for nice look , splash screen added , Roboto Font added and used on the interface

---
+++ studioactions/mobile/AS_AppEvents_jc795d20b1504fa9a54eb24807a6d96d.js
@@ -0,0 +1,8 @@
+function AS_AppEvents_jc795d20b1504fa9a54eb24807a6d96d(eventobject) {
+    var self = this;
+    kony.application.setApplicationProperties({
+        statusBarColor: "FFFFFF",
+        statusBarHidden: false,
+        statusBarStyle: constants.STATUS_BAR_STYLE_DEFAULT,
+    });
+}
