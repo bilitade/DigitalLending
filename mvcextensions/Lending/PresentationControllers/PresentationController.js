07689b15d1a13561d09ddf1f8e8dadf50f1fc65f
Gutu Rarie <guturarie@gmail.com>
2025-03-03 08:52:02 +0300

    Update with Main

--- mvcextensions/Lending/PresentationControllers/PresentationController.js
+++ mvcextensions/Lending/PresentationControllers/PresentationController.js
@@ -44,5 +44,39 @@ define([], function () {
         kony.print("Exiting PresentationController.fetchUserDetails");
     };
     
+  // Now fetchUserDetails accepts success and error callbacks
+    PresentationController.prototype.fetchLoanDetails = function (customerID,successCallback, errorCallback) {
+        kony.print("Entering PresentationController.fetchUserDetails");
+    
+        // Get the callbacks from this method’s parameters
+        var lendingModule = kony.mvc.MDAApplication.getSharedInstance()
+                              .getModuleManager()
+                              .getModule("LendingManager"); // Ensure this is the correct module name
+        var businessController = lendingModule.businessController;
+    
+        if (!businessController) {
+            kony.print("BusinessController is not initialized.");
+            return;
+        }
+    
+        // Call the business controller and pass the callbacks
+        businessController.fetchLoanDetails(customerID,
+            function (response) {
+                kony.print("fetchUserDetails Success");
+                // Directly call the success callback provided by the Form Controller
+                if (successCallback && typeof successCallback === "function") {
+                    successCallback(response);
+                }
+            },
+            function (error) {
+                kony.print("fetchLoanDetails Failed: " + JSON.stringify(error));
+                if (errorCallback && typeof errorCallback === "function") {
+                    errorCallback(error);
+                }
+            }
+        );
+    
+        kony.print("Exiting PresentationController.fetchUserDetails");
+    };
     return PresentationController;
 });
