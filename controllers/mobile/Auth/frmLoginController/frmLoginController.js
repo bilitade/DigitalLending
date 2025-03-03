e46ed876553921df5e3c42e518612439c525cca8
Bilisuma <bilitade2022@gmail.com>
2025-03-03 13:34:42 +0300

    login ui redesign for nice look

--- controllers/mobile/Auth/frmLoginController/frmLoginController.js
+++ controllers/mobile/Auth/frmLoginController/frmLoginController.js
@@ -1,5 +1,24 @@
-define({ 
+define({
+  onLoginBtnClick: function() {
+    var basicConfig = {
+      message: "Invalid Credentials",
+      alertTitle: "Login",
+      alertType: constants.ALERT_TYPE_ERROR,
+      yesLabel: "OK"
+    };
 
- //Type your controller code here 
+    let email = this.view.emailTextBox.text;
+    let password = this.view.passwordTextBox.text;
 
- });
+    if (email && password) {
+      kony.ui.Alert({
+        message: "Login credentials are correct!",
+        alertTitle: "Login",
+        alertType: constants.ALERT_TYPE_INFO,
+        yesLabel: "OK"
+      }, {});
+    } else {
+      kony.ui.Alert(basicConfig, {});
+    }
+  }
+});
--- studioactions/mobile/appEvents.js
+++ studioactions/mobile/appEvents.js
@@ -2,10 +2,10 @@ define({
     /*
         This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
-    AS_AppEvents_jc795d20b1504fa9a54eb24807a6d96d: function AS_AppEvents_jc795d20b1504fa9a54eb24807a6d96d(eventobject) {
+    AS_AppEvents_h3159bb6377849848a653d96d1b01e58: function AS_AppEvents_h3159bb6377849848a653d96d1b01e58(eventobject) {
         var self = this;
         kony.application.setApplicationProperties({
-            statusBarColor: "FFFFFF",
+            statusBarColor: '00adef',
             statusBarHidden: false,
             statusBarStyle: constants.STATUS_BAR_STYLE_DEFAULT,
         });
